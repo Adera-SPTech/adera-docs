a0175936772611de7bbc6db71e892822a41dffb6
Renan2805 <renansilva.dev@gmail.com>
2023-10-23 16:35:07 -0300

    Atualiza docs

--- dataFolder/script-adera.sql
+++ dataFolder/script-adera.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS adera.estabelecimento (
   PRIMARY KEY (id))
 ;
 
+<<<<<<< Updated upstream
 -- -----------------------------------------------------
 -- Table adera.maquina
 -- -----------------------------------------------------
@@ -74,12 +75,91 @@ CREATE TABLE IF NOT EXISTS adera.maquinacomponente (
     REFERENCES adera.tipocomponente (id));
 
 
+=======
+INSERT INTO adera.estabelecimento VALUES ('78c0330e-70f7-11ee-b962-0242ac120002', 'Walmart', '24616269000165');
+
+-- -----------------------------------------------------
+-- Table adera.maquina
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS adera.maquina (
+  id CHAR(36) NOT NULL,
+  os VARCHAR(45) NOT NULL,
+  fabricante VARCHAR(45) NOT NULL,
+  arquitetura INT NOT NULL,
+  enderecoMac varchar(45) not null,
+  fkEstabelecimento CHAR(36) NOT NULL,
+  PRIMARY KEY (id),
+  INDEX fkEstabelecimento (fkEstabelecimento ASC) VISIBLE,
+  CONSTRAINT maquina_ibfk_1
+    FOREIGN KEY (fkEstabelecimento)
+    REFERENCES adera.estabelecimento (id))
+;
+
+
+-- -----------------------------------------------------
+-- Table adera.unidademedida
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS adera.unidademedida (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(15) NOT NULL,
+  abreviacao VARCHAR(5) NOT NULL,
+  PRIMARY KEY (id));
+
+INSERT INTO adera.unidadeMedida VALUES 
+	(null, 'byte', 'b'),
+    (null, 'porcentagem', '%'),
+    (null, 'heartz', 'hz');
+
+-- -----------------------------------------------------
+-- Table adera.tipocomponente
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS adera.tipocomponente (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  fkUnidadeMedida INT NOT NULL,
+  PRIMARY KEY (id),
+  INDEX fkUnidadeMedida (fkUnidadeMedida ASC) VISIBLE,
+  CONSTRAINT tipocomponente_ibfk_1
+    FOREIGN KEY (fkUnidadeMedida)
+    REFERENCES adera.unidademedida (id));
+
+INSERT INTO adera.tipocomponente VALUES
+	(null, 'cpu', 2),
+    (null, 'memory', 1),
+    (null, 'disk', 1);
+
+-- -----------------------------------------------------
+-- Table adera.maquinacomponente
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS adera.maquinacomponente (
+  id CHAR(36) NOT NULL,
+  modelo VARCHAR(90) NOT NULL,
+  descricao VARCHAR(45) NOT NULL,
+  fkMaquina CHAR(36) NOT NULL,
+  fkTipoComponente INT NOT NULL,
+  PRIMARY KEY (id),
+  INDEX fkMaquina (fkMaquina ASC) VISIBLE,
+  INDEX fkTipoComponente (fkTipoComponente ASC) VISIBLE,
+  CONSTRAINT maquinacomponente_ibfk_1
+    FOREIGN KEY (fkMaquina)
+    REFERENCES adera.maquina (id),
+  CONSTRAINT maquinacomponente_ibfk_2
+    FOREIGN KEY (fkTipoComponente)
+    REFERENCES adera.tipocomponente (id));
+
+
+>>>>>>> Stashed changes
 -- -----------------------------------------------------
 -- Table adera.metrica
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS adera.metrica (
   id CHAR(36) NOT NULL,
+<<<<<<< Updated upstream
   medicao INT NOT NULL,
+=======
+  medicao VARCHAR(180) NOT NULL,
+  data DATETIME NOT NULL,
+>>>>>>> Stashed changes
   fkMaquinaComponente CHAR(36) NOT NULL,
   PRIMARY KEY (id),
   INDEX fkMaquinaComponente (fkMaquinaComponente ASC) VISIBLE,
@@ -137,3 +217,8 @@ CREATE TABLE IF NOT EXISTS adera.usuario (
   CONSTRAINT usuario_ibfk_1
     FOREIGN KEY (fkEstabelecimento)
     REFERENCES adera.estabelecimento (id));
+<<<<<<< Updated upstream
+=======
+    
+INSERT INTO adera.usuario VALUES ('9f2eea3a-70f7-11ee-b962-0242ac120002', 'renansilva.dev@gmail.com', 'aditum123', 'Renan', 'Silva', 'Tecnico', '78c0330e-70f7-11ee-b962-0242ac120002');
+>>>>>>> Stashed changes
