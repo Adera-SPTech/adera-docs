cb0774d64fee08c1ed4a5d1970af661f333ba58d
icaro-bezerra <icaro.bezerra@sptech.school>
2023-11-16 21:30:23 -0300

    autoCommit

--- script-instalacao-adera-scanner.sh
+++ script-instalacao-adera-scanner.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 sudo apt update
-
 if command docker  &> /dev/null
 then
     echo "Docker já está instalado."
 else
-    # Ask the user if they want to install java
     read -p "Ambiente não está instalado, deseja instalar o ambiente? (s/n): " res
     if [ "$res" == "s" ]||[ "$res" == "S" ]||[ "$res" == "SIM" ]||[ "$res" == "sim" ]||[ "$res" == "Sim" ]; then
-        # Install Java
         sudo apt install docker.io -y
         echo "Ambiente instalado com sucesso."
     else
@@ -17,9 +14,7 @@ else
         exit 1
     fi
 fi
-
 sudo docker run -d -p 3306:3306 aderatech/db:latest
-
 sleep 5
 clear
 sudo docker run -it aderatech/jar:latest
